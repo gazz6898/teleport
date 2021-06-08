11b626a5f530641bf82eb6918c6309bc019bbe77
Brandon Peterson <brandonman17@gmail.com>
2021-06-07 20:24:56 -0400

    Add "/query" POST request

--- packages/backend/src/index.js
+++ packages/backend/src/index.js
@@ -20,11 +20,52 @@ mysql
 
     // REST API
     app.get('/', async (req, res) => {
+
+      console.log('==================================================\nGET\n/');
+      console.log(req.body);
+      console.log('--------------------------------------------------');
+
       res.json('Hello, world!');
+      console.log('==================================================');
+    });
+
+    app.post('/query', async (req, res) => {
+      console.log('==================================================\nPOST\n/');
+      console.log(req.body);
+      console.log('--------------------------------------------------');
+      let query = await getTable(sql, req.body.tableName, req.body.columns);
+      console.log(query);
+      res.json(query);
+      console.log('==================================================');
     });
 
     app.post('/login', async (req, res) => {
-      res.json(req.body);
+      
+      console.log('==================================================\nPOST\n/login');
+      console.log(req.body);
+      console.log('--------------------------------------------------');
+
+      let userQuery = await sql.query('SELECT * FROM `User` WHERE username = ?', [req.body.email]).catch(error => console.log(error));
+
+      if(!userQuery.length) {
+        //user not found
+        console.log('user not found\n', {email: req.body.email});
+
+      } else if(userQuery[0].pass_hash != req.body.password) {
+        //incorrect password
+        console.log('incorrect password\n', {email: req.body.email, passhash: userQuery[0].pass_hash, attempthash: req.body.password});
+
+      } else {
+        //correct password
+        console.log('correct password\n', {email: req.body.email, passhash: req.body.password});
+      }
+
+      if(userQuery.length) console.log(userQuery[0].username);
+
+      res.json(userQuery);
+      //res.json(req.body);
+
+      console.log('==================================================');
     });
 
     app.listen(config.get('server.port'), () => {
@@ -35,3 +76,20 @@ mysql
     console.log(error);
     process.exit(1);
   });
+
+ /**
+  * This function returns an array of rows with the given column names from the given table
+  * @param {mysql.Connection} sql 
+  * @param {string} tableName 
+  * @param {string[]} columns 
+  * @returns {any[]}
+  */
+  function getTable(sql, tableName, columns) {
+
+    let queryString = `SELECT ${columns.length ? columns.join(', ') : '*'} FROM ${tableName}`;
+
+    return sql.query(queryString).catch(error => console.log(error));
+  }
+
+
+
